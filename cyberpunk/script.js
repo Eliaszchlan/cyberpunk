1e5e3b400b604b51d6501f9f7bc91c5fc1fe0b31
Eliaszchlan <81645733+Eliaszchlan@users.noreply.github.com>
2023-06-28 15:50:29 +0300

    first_commit
    first_commit

---
+++ cyberpunk/script.js
@@ -0,0 +1,24 @@
+const swiper = new Swiper('.swiper-container', {
+    loop: true,
+    autoplay: {
+      delay: 5000,
+    },
+    effect: 'fade',
+    fadeEffect: {
+        crossFade: true
+    },
+  });
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
